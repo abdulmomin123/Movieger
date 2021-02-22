73f5d406a7485fdee944c6446e78e4e72f356dad
Abdul Momin <abdulmomin114477@gmail.com>
2021-02-22 17:49:28 +0600

    Removed all defaults & restructured folders & cleanded up _app & index

---
+++ src/pages/_app.tsx
@@ -0,0 +1,7 @@
+import { AppProps } from 'next/dist/next-server/lib/router/router';
+
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+);
+
+export default MyApp;
